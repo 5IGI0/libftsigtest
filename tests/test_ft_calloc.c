4d7760f8721c2eb15a75dfcae1d57744345dbc00
elorenze <5igi0@protommail.com>
2022-03-22 17:27:38 +0100

    ft_calloc: check for non-null return on 0 bytes

--- tests/test_ft_calloc.c
+++ tests/test_ft_calloc.c
@@ -17,23 +17,32 @@ int main(void) {
     
     STDOUT_WRITE(CLR_RST"ft_calloc\t: ");
 
-    /* 1. malloc error */
-    SET_MMX(0);
-    ptr = ft_calloc(1, 1);
+    /* 1. check for 0 bytes allocation */
+    SET_MMX(1);
+    ptr = ft_calloc(0, 0);
     UNSET_MMX();
-    if (ptr)
-        STDOUT_WRITE(CLR_RED"1.KO (non-null return) ");
+    if (!ptr)
+        STDOUT_WRITE(CLR_RED"1.KO (NULL instead malloc return) ");
     else
         STDOUT_WRITE(CLR_GRN"1.OK ");
 
-    /* 2. integer overflow */
-    SET_MMX(1);
-    ptr = ft_calloc((((size_t)-1)>>(sizeof(size_t)>>2)) + 1, (((size_t)-1)>>(sizeof(size_t)>>2)) + 1);
+    /* 2. malloc error */
+    SET_MMX(0);
+    ptr = ft_calloc(1, 1);
     UNSET_MMX();
     if (ptr)
         STDOUT_WRITE(CLR_RED"2.KO (non-null return) ");
     else
         STDOUT_WRITE(CLR_GRN"2.OK ");
+
+    /* 3. integer overflow */
+    SET_MMX(1);
+    ptr = ft_calloc((((size_t)-1)>>(sizeof(size_t)>>2)) + 1, (((size_t)-1)>>(sizeof(size_t)>>2)) + 1);
+    UNSET_MMX();
+    if (ptr)
+        STDOUT_WRITE(CLR_RED"3.KO (non-null return) ");
+    else
+        STDOUT_WRITE(CLR_GRN"3.OK ");
 
     STDOUT_WRITE(CLR_RST"\n");
 }
